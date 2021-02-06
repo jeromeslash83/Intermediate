3c2841a8285204233dae9a18bc4605f373179462
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-06 22:20:05 +0800

    Create Coffee_machine.py

---
+++ Coffee_machine.py
@@ -0,0 +1,43 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+water = 0
+milk = 0
+coffee = 0
+
+def check_resources():
+    """Checks if the resources are in the machine are sufficient."""
+
+
+
+order = input("What would you like? (espresso/latte/cappuccino):\n")
