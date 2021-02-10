19a8526055d234709cb421d6e9e8ce61dddaa4bf
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-10 22:40:46 +0800

    Update main.py

--- Coffee_machine_code/OOP_Version/main.py
+++ Coffee_machine_code/OOP_Version/main.py
@@ -3,24 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 resources = CoffeeMaker()
-menu = Menu()
+my_menu = Menu()
 money = MoneyMachine()
 is_on = True
 
 while is_on:
-    order = input(f"What would you like? {menu.get_items()} \n").lower()
+    order = input(f"What would you like? {my_menu.get_items()} \n").lower()
     if order == 'report':
         resources.report()
         money.report()
     elif order == 'off':
         is_on = False
     else:
-        choice = menu.find_drink(order)
-        if resources.is_resource_sufficient(choice):
-           if money.make_payment(choice.cost):
+        if resources.is_resource_sufficient(my_menu.find_drink(order)):
+           if money.make_payment(my_menu.find_drink(order).cost):
                pass
            else:
                is_on = False
-           resources.make_coffee(choice)
+           resources.make_coffee(my_menu.find_drink(order))
         else:
             is_on = False
