a08d577dbf3f71b74a26e9a2552437c6e080743a
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-16 00:25:22 +0800

    Update Question Game.py

--- Quiz Game OOP/Question Game.py
+++ Quiz Game OOP/Question Game.py
@@ -1,15 +1,17 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+import random
 
 question_bank = []
 
 for dict in question_data:
-    question = dict['text']
-    answer_to_question = dict['answer']
+    question = dict['question']
+    answer_to_question = dict['correct_answer']
     question_bank.append(Question(question, answer_to_question))
 
-quiz = QuizBrain(question_bank)
+questions = random.sample(question_bank, 12)
+quiz = QuizBrain(questions)
 
 while quiz.still_has_questions():
     quiz.next_question()
