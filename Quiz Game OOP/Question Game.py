2f13a18e2542d8bf9234ecc928399edcb75867f2
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-15 23:53:44 +0800

    Update Question Game.py

--- Quiz Game OOP/Question Game.py
+++ Quiz Game OOP/Question Game.py
@@ -1,8 +1,6 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
-import random
-
 
 question_bank = []
 
@@ -12,3 +10,10 @@ for dict in question_data:
     question_bank.append(Question(question, answer_to_question))
 
 quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+    
+print("You've completed the quiz!")
+print(f"Your final score was {quiz.score} / {quiz.question_number}.")
+        
