4981a0416618e08a0267575e68fe36771e6ca9ef
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-15 23:53:04 +0800

    Update quiz_brain.py

--- Quiz Game OOP/quiz_brain.py
+++ Quiz Game OOP/quiz_brain.py
@@ -3,10 +3,28 @@ class QuizBrain:
     def __init__(self, all_questions):
         self.question_number = 0
         self.questions_list = all_questions
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
 
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        current_question = input(f"Q.{self.question_number}: {current_question.text}: (True/False)\n")
-        
+        user_answer = input(f"Q.{self.question_number}: {current_question.text}: (True/False)\n").lower()
+        self.check_answer(user_answer, current_question.answer)
 
+    def check_answer(self, user_answer, answer):
+        if self.question_number < 12:
+            if user_answer == answer:
+                self.score += 1
+                print("Correct!")
+            else:
+                print("Wrong.")
+        else:
+            pass
+
+        print(f"The correct answer is: {answer}.")
+        print(f"Your current score is {self.score}/{self.question_number}.")
+
+        
