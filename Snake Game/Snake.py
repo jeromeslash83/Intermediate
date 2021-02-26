8e469eb834b6484ac109d15c244bdeac1c96e173
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-26 21:31:18 +0800

    Update Snake.py

--- Snake Game/Snake.py
+++ Snake Game/Snake.py
@@ -1,1 +1,18 @@
 #Main
+from turtle import Turtle, Screen
+import random
+
+
+
+screen= Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Jslash Snake Game")
+
+
+snake_pos = [(0,0),(-20,0),(-40,0)]
+for position in snake_pos:
+    snake = Turtle(shape="square")
+    snake.color("green")
+    snake.goto(position)
+
