f5c20de76d065f5531899334d17a3db1e250bc9f
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-26 22:46:16 +0800

    Update Snake.py

--- Snake Game/Snake.py
+++ Snake Game/Snake.py
@@ -1,20 +1,28 @@
 #Main
 from turtle import Turtle, Screen
 import random
-
-
+import time
 
 screen= Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Jslash Snake Game")
+screen.tracer(0)
 
 x = 0
 the_snake = []
 for _ in range(3):
     snake = Turtle(shape="square")
     snake.color("green")
+    snake.penup()
     snake.goto(x=x, y=0)
     x -= 20
     the_snake.append(snake)
+
+snake_moving = True
+
+while snake_moving:
+    for segment in the_snake:
+        segment.fd(20)
+        screen.update()
 
