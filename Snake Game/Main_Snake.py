717c66f2d1f72d8efbbf01d44c427a4435d2668a
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-28 17:20:31 +0800

    Update Main_Snake.py

--- Snake Game/Main_Snake.py
+++ Snake Game/Main_Snake.py
@@ -1,7 +1,9 @@
 #Main Code
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from Snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +12,8 @@ screen.title("Jslash Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,9 +23,24 @@ screen.onkey(snake.left, "Left")
 
 snake_is_alive = True
 while snake_is_alive:
+    score.scoreboard()
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.snake_head.distance(food) < 15:
+        food.refresh()
+        snake.add_length()
+        score.add_score()
+    
+    if snake.snake_head.xcor() > 290 or snake.snake_head.xcor() < -290 or snake.snake_head.ycor() > 290  or snake.snake_head.ycor() < -290:
+        snake_is_alive = False
+        score.gameover()
 
+    for snake_part in snake.the_snake:
+        if snake_part == snake.snake_head:
+            pass
+        elif snake.snake_head.distance(snake_part) < 10:
+            snake_is_alive = False
+            score.gameover()
 
 screen.exitonclick()
