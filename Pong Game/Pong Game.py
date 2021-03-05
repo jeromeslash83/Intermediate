310fa6d0b685cea4f1d5b187c7557121d30cb936
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-03-05 10:40:08 +0800

    Update Pong Game.py

--- Pong Game/Pong Game.py
+++ Pong Game/Pong Game.py
@@ -1,9 +1,32 @@
 from turtle import Turtle, Screen
+from paddles import Paddle
 import time
-from paddles import Paddle()
+
 
 screen = Screen()
 screen.setup(height=600, width=800)
 screen.bgcolor("black")
 screen.title("Jerome's Pong Game")
 screen.tracer(0)
+
+
+
+paddle = Paddle()
+
+screen.listen()
+screen.onkey(paddle.move_up_1, "Up")
+screen.onkey(paddle.move_down_1, "Down")
+screen.onkey(paddle.move_up_2, "w")
+screen.onkey(paddle.move_down_2, "s")
+
+
+game_is_ongoing = True
+while game_is_ongoing:
+    screen.update()
+    time.sleep(0.1)
+
+
+
+
+
+screen.exitonclick()
