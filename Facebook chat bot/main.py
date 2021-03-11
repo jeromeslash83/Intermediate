3e43d216247ace0b4b7c2d6790663f1e422207af
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-03-11 15:29:53 +0800

    Update main.py

--- Facebook chat bot/main.py
+++ Facebook chat bot/main.py
@@ -1,2 +1,11 @@
 #FB Page bot
 from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def receive_message():
+    return "Hello Jerome!"
+
+if __name__ == '__main__':
+    app.run()
