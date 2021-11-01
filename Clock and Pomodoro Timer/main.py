c7cd46b2fce181005a2973ced2c34875a89a9432
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-11-01 17:25:12 +0800

    Create main.py

---
+++ Clock and Pomodoro Timer/main.py
@@ -0,0 +1,69 @@
+import tkinter
+from tkinter import messagebox
+import time
+import winsound
+
+#This deals with the main interface of the app.
+interface = tkinter.Tk()
+interface.configure(background='White')
+interface.title('Timer')
+interface.geometry('600x400')
+
+
+#this defines the app and runs the clock
+def the_clock():
+    """Sets the clock to 24 hour time"""
+    hour = time.strftime('%H')
+    minutes = time.strftime('%M')
+    seconds = time.strftime('%S')
+    day = time.strftime('%D')
+
+    label.config(text= f'{hour}:{minutes}:{seconds}')
+    label.after(1000, the_clock)
+
+    label2.config(text= f'{day}')
+
+
+label = tkinter.Label(interface, text= '', font=('Helvetica', 48), foreground='Black', background='White')
+label.pack(pady=30)
+label2 = tkinter.Label(interface, text='', font=('Helvetica', 20), foreground='Black', background='White')
+label2.pack(pady=0)
+
+
+#this runs the pomodoro function of the app.
+def start_pomodoro():
+    """Starts the pomodoro time to 50/10 intervals"""
+    while True:
+        for i in range(3000, -1, -1): 
+            minute = i // 60
+            seconds = i % 60
+            pomodoro_label.config(text=f'{minute}:{seconds}')
+            interface.update()
+            time.sleep(1)
+        
+        for i in range(5):
+            winsound.Beep(i+100, 500)
+
+        messagebox.showinfo("Break time", '10 minute break')
+
+        for i in range(600,-1,-1):
+            minute = i // 60
+            seconds = i % 60
+            pomodoro_label.config(text=f'{minute}:{seconds}')
+            interface.update()
+            time.sleep(1)
+         
+        continuation = messagebox.askyesno('Do you want to continue?')
+        if continuation == True: continue
+        else: break
+
+    
+
+pomodoro_button = tkinter.Button(interface, text='Start Pomodoro', background='Orange', foreground='Black', command=start_pomodoro)
+pomodoro_button.pack(padx=10, pady=10)
+pomodoro_label = tkinter.Label(interface, text='', font=('Helvetica', 48))
+pomodoro_label.pack(padx=10, pady=10)
+
+
+the_clock()
+interface.mainloop()
